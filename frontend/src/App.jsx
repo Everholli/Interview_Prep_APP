fcc20dca4d92870410d93a2b1badd00fa5cdd39f
Everholli <shrutimishra2522@gmail.com>
2025-09-29 13:16:13 +0530

    feat: implement routing and add Interview Preparation page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import Login from "./pages/Auth/Login";
+import SignUp from "./pages/Auth/SignUp";
+import Dashboard from "./pages/Home/Dashboard";
+import Interview_Prep from "./pages/interview_Prep/interview_Prep";
+import LandingPage from "./pages/LandingPage";
 
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl text-red-900 font-bold">Hello, World!</h1>
-      <p className="mt-4 text-pink-700">Welcome to the React application.</p>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/interview-prep/:sessionId" element={<Interview_Prep />} />
+        </Routes>
+      </Router>
+
+      <Toaster 
+        toastOptions={{
+          className: "",
+          style: {
+            border: "1px solid #713200",
+            padding: "16px",
+            fontsize: "16px",
+          },
+        }}
+      />
     </div>
   );
 }
---
+++ frontend/src/pages/interview_Prep/interview_Prep.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Interview_Prep = () => {
+  return (
+    <div>Interview Preparation Page</div>
+  );
+}
+
+export default Interview_Prep;
